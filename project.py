b641dd4e5783fbd014c4dd2d31104b0cf630b59f
ElReyZero <juan.romero1201@gmail.com>
2022-10-26 11:33:18 -0500

    Docs: Add documentation of objective function and problem constraints

--- project.py
+++ project.py
@@ -69,14 +69,31 @@ model.x = Var(model.P, model.L, domain=Binary)  # Product selected
 model.y = Var(model.L, domain=Binary)  # Purchase in shop
 
 # Objective function
+"""The objective function seeks to minimize the sum of the costs of the chosen products for all stores.
+If a store has at least one product chosen, the binary variable Yl will allow us to include to the final sum of the shipping cost corresponding to that store.
+"""
 model.targetFunc = Objective(expr=sum(sum(model.Costs[j, l]*model.x[j, l] for j in model.P) + model.DeliveryCosts[l] * model.y[l] for l in model.L), sense=minimize)
 
 # Constraints
 def activate_y_if_l_is_chosen(model, l):
+    """This restriction indicates that if at least one product is chosen from a store l,
+    the variable Y must be activated, since it means that a purchase
+    is being made in store l.
+    """
     return sum(model.x[j, l] - len(model.P)*model.y[l] for j in model.P) <= 0
 
 
 def wanted_products_must_be_bought(model, j):
+    """This restriction indicates that for each available product that the buyer wants, exactly 1 of them must be purchased. 
+    That is, it is not possible to buy more than one specific product per store.
+
+    Args:
+        model (_type_): _description_
+        j (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return sum(model.x[j, l] for l in model.L) == 1
 
 
